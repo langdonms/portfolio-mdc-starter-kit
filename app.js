7d9a9d988b92b93faf138c12a0b17ceb71530819
LangdonMS <langdon.sanders@gmail.com>
2019-03-24 12:41:39 -0400

    added JavaSrtip instantiation using example for Drawer component to app.js file

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 import {MDCRipple} from '@material/ripple/index';
+import {MDCList} from "@material/list";
 
 const ripple = new MDCRipple(document.querySelector('.foo-button'));
+
+const list = MDCList.attachTo(document.querySelector('.mdc-list'));
+list.wrapFocus = true;
