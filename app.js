f4b95e310d2e53e54338bd8fb47fa39742902a41
LangdonMS <langdon.sanders@gmail.com>
2019-03-25 13:49:58 -0400

    edits to app.js and app.scss to try to implement Drawer component

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import {MDCRipple} from '@material/ripple/index';
+import {MDCDrawer} from "@material/drawer";
 import {MDCList} from "@material/list";
 
 const ripple = new MDCRipple(document.querySelector('.foo-button'));
 
-const list = MDCList.attachTo(document.querySelector('.mdc-list'));
-list.wrapFocus = true;
+const drawer = MDCDrawer.attachTo(document.querySelector('.mdc-drawer'));
